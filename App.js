264174961f83680f371125ef7da7dee2bb475f6d
Chris Vanderloo <chris.vanderloo@yahoo.com>
2019-01-18 19:30:35 -0500

    Switched to tab bar controller

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Tabs } from './navigation'
+
+
+export default class App extends React.Component {
+  render() {
+    return ( <Tabs /> );
+  }
+} 
