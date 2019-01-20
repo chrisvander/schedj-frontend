44afffe1cf56ed3571f17be63495532313829727
Chris Vanderloo <chris.vanderloo@yahoo.com>
2019-01-20 18:35:11 -0500

    styling and assets

---
+++ styles/login.js
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+		rensselaerText: {
+			paddingTop: 10,
+			color: '#677791',
+			fontSize: 18,
+			fontWeight: '500',
+			fontFamily: 'Helvetica Neue',
+		},
+		sisText: {
+			color: '#AC3939',
+			fontWeight: 'bold',
+			fontSize: 21,
+			fontFamily: 'Helvetica Neue',
+			paddingBottom: 8,
+		},
+		textInputContainer: {
+			borderColor: '#BCE0FD',
+			borderWidth: 1,
+			marginTop: 16
+		},
+		textInput: {
+			padding: 16,
+			color: '#2699FB',
+		},
+		buttonContainer: {
+			borderRadius: 7,
+			backgroundColor: '#2699FB',
+			marginTop: 16,
+			marginBottom: 16
+		},
+		button: {
+			padding: 16.5,
+			color: 'white',
+			fontSize: 12,
+			fontWeight: 'bold'
+		},
+		privacy: {
+			fontFamily: 'Arial',
+			fontSize: 10,
+			color: '#2699FB'
+		}
+});
