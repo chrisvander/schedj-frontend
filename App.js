b30d0803b07d7082a092fcb70a4137849caf35d4
Chris Vanderloo <chris.vanderloo@yahoo.com>
2019-01-20 18:34:03 -0500

    re routed to login view

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { Tabs } from './navigation'
+import { Login } from './views'
 
 
 export default class App extends React.Component {
   render() {
-    return ( <Tabs /> );
+    return ( <Login /> );
   }
 } 
