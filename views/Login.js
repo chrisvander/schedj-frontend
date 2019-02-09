8f54f5ec41e25580b75449bc7ef3b80a2cebe4e5
Chris Vanderloo <chris.vanderloo@yahoo.com>
2019-02-09 02:11:49 -0500

    Added loading screen on login to indicate status

--- views/Login.js
+++ views/Login.js
@@ -9,7 +9,7 @@ import {
   View, 
   TextInput, 
   KeyboardAvoidingView,
-  TouchableWithoutFeedback
+  ActivityIndicator
 } from 'react-native';
 import Button from 'react-native-button';
 import { RoundedCard } from '../components';
@@ -20,15 +20,17 @@ import { signIn } from '../auth';
 export default class FeedScreen extends React.Component {
   componentWillMount() {
     this.sis_man = (<Image style={{ marginTop: 43, marginBottom: 30 }} source={require('../assets/sis_man.png')} />);
-    this.setState({ username: '', password: ''});
+    this.setState({ username: '', password: '', loading: false });
   }
 
   login(nav) {
+    this.setState({ loading: true });
     signIn(this.state.username, this.state.password)
       .then((valid) => {
         nav('Home');
       }).catch((err) => {
         alert(err);
+        this.setState({ loading: false });
       });
   }
 
@@ -36,18 +38,18 @@ export default class FeedScreen extends React.Component {
     const {navigate, goBack} = this.props.navigation;
     return (
       <View style={{ flex: 1, justifyContent: 'flex-end', height: '100%' }}>
-      	<LinearGradient
+        <LinearGradient
           colors={['#FFFFFF', '#53B7FD']}
           style={{ padding: 24, alignItems: 'center', height: '100%' }}
           start={[0.33,0.33]}>
           <SafeAreaView style={{ alignItems: 'center', height: '100%', width:'100%' }}>
-          	{this.sis_man}
+            {this.sis_man}
             <KeyboardAvoidingView keyboardVerticalOffset={-100} style={{flex: 1}} behavior="position" enabled>
-  	          <RoundedCard style={{padding:23}}>
-  	          	<Text style={[LoginStyle.rensselaerText]}>Rensselaer's</Text>
-  	          	<Text style={[LoginStyle.sisText]}>Student Information System</Text>
-  	          	<View style={[LoginStyle.textInputContainer]}>
-  	          		<TextInput 
+              <RoundedCard style={{padding:23}}>
+                <Text style={[LoginStyle.rensselaerText]}>Rensselaer's</Text>
+                <Text style={[LoginStyle.sisText]}>Student Information System</Text>
+                <View style={[LoginStyle.textInputContainer]}>
+                  <TextInput 
                     placeholderTextColor='#BCE0FD' 
                     style={[LoginStyle.textInput]} 
                     placeholder='RIN' 
@@ -58,9 +60,9 @@ export default class FeedScreen extends React.Component {
                     onChangeText={(username) => this.setState({username})}
                     blurOnSubmit={false}
                   />
-  	          	</View>
-  	          	<View style={[LoginStyle.textInputContainer]}>
-  	          		<TextInput 
+                </View>
+                <View style={[LoginStyle.textInputContainer]}>
+                  <TextInput 
                     placeholderTextColor='#BCE0FD' 
                     style={[LoginStyle.textInput]} 
                     placeholder='Password' 
@@ -71,23 +73,28 @@ export default class FeedScreen extends React.Component {
                     onSubmitEditing={()=>this.login(navigate)}
                     onChangeText={(password) => this.setState({password})}
                   />
-  	          	</View>
-            		<Button 
-            			activeOpacity={0.7}
-            			color='#FFFFFF' 
-            			style={[LoginStyle.button]} 
-            			containerStyle={[LoginStyle.buttonContainer]}
-            			onPress={()=>this.login(navigate)}
-            		>
-            			LOGIN
-            		</Button>
-            		<View style={{justifyContent: 'center', alignItems: 'center'}}>
-  		          	<Text style={[LoginStyle.privacy]}>By logging into SIS, you agree to our {"\n"} Terms of Service and Privacy Policy</Text>
-  	          	</View>
-  	          </RoundedCard>
+                </View>
+                <Button 
+                  activeOpacity={0.7}
+                  color='#FFFFFF' 
+                  style={[LoginStyle.button]} 
+                  containerStyle={[LoginStyle.buttonContainer]}
+                  onPress={()=>this.login(navigate)}
+                >
+                  LOGIN
+                </Button>
+                <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                  <Text style={[LoginStyle.privacy]}>By logging into SIS, you agree to our {"\n"} Terms of Service and Privacy Policy</Text>
+                </View>
+              </RoundedCard>
             </KeyboardAvoidingView>
-	        </SafeAreaView>
+          </SafeAreaView>
         </LinearGradient>
+        { this.state.loading &&
+          <View style={[LoginStyle.overlay]}>
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View>
+        }
       </View>
     );
   }
