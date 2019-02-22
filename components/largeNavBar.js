5326b5388527082557ee0d658e1aa7e37031adf0
Chris Vanderloo <chris.vanderloo@yahoo.com>
2019-02-22 17:01:44 -0500

    SIS went down so I added error handling

--- components/largeNavBar.js
+++ components/largeNavBar.js
@@ -9,25 +9,27 @@ export default class LargeNavBar extends React.Component {
 	}
 
 	render() {
-		var shadow = {shadowOpacity: 0.16};
+		var shadow = {shadowOpacity: 0.16, shadowRadius: 10};
 		if (this.props.shadow == false) shadow = {shadowOpacity: 0.0};
 		return (
-			<View style={[ NavStyle.largeNavBarContainer, shadow ]}>
+			<View style={[ shadow ]}>
 				<View style={{backgroundColor:'#FFFFFF'}}>
-					<SafeAreaView style={[ NavStyle.largeNavBarView ]}>
-						<View style={{flexDirection: 'column', justifyContent: 'flex-end'}}>
-							<Text style={[NavStyle.subTitle]}>{this.props.preTitle}</Text>
-							<Text style={[ NavStyle.bigTitle ]}>{this.props.title}</Text>
-						</View>
-						<View style={[ NavStyle.gearContainer ]}>
-							<TouchableOpacity onPress={this.settings}>
-								<Image 
-									style={[ NavStyle.gear ]} 
-									source={require('../assets/icons/gear.png')} 
-									defaultSource={require('../assets/icons/gear.png')}
-									resizeMode="cover"
-								/>
-							</TouchableOpacity>
+					<SafeAreaView>
+						<View style={[NavStyle.largeNavBarView]}>
+							<View style={{flexDirection: 'column', justifyContent: 'flex-end'}}>
+								<Text style={[NavStyle.subTitle]}>{this.props.preTitle}</Text>
+								<Text style={[ NavStyle.bigTitle ]}>{this.props.title}</Text>
+							</View>
+							<View style={[ NavStyle.gearContainer ]}>
+								<TouchableOpacity onPress={this.settings}>
+									<Image 
+										style={[ NavStyle.gear ]} 
+										source={require('../assets/icons/gear.png')} 
+										defaultSource={require('../assets/icons/gear.png')}
+										resizeMode="cover"
+									/>
+								</TouchableOpacity>
+							</View>
 						</View>
 					</SafeAreaView>
 				</View>
