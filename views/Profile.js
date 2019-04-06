bb7b9f43021d18c802c4809486a5abf59ac00771
Chris Vanderloo <chris.vanderloo@yahoo.com>
2019-04-06 01:18:44 -0400

    Grades feature implementation

--- views/Profile.js
+++ views/Profile.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { Text, View, Button, ScrollView } from 'react-native';
-import { LargeNavBar } from '../components';
+import { Text, View, Button, ScrollView, StyleSheet } from 'react-native';
+import { LargeNavBar, RoundedCard, RoundedCardTitle } from '../components';
+import { FN } from '../styles';
+
+const styles = StyleSheet.create({
+  gradesCard: {
+    padding: FN(10),
+    paddingLeft: 20,
+    paddingRight: 20,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 17
+  },
+});
 
 export default class ProfileScreen extends React.Component {
   render() {
     return (
     	<React.Fragment>
     		<LargeNavBar navigation={this.props.navigation} title="Profile" />
-	      <ScrollView>
-	        
+	      <ScrollView style={{padding: 16, paddingTop: 30}}>
+          <RoundedCard 
+            style={[styles.gradesCard]} 
+            color={'blue'} 
+            caret={true} 
+            title={'Grades'} 
+            onPress={()=>this.props.navigation.navigate('Grades')}
+          />
 	      </ScrollView>
 	     </React.Fragment>
     );
