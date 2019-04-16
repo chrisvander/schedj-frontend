67e820667ccc2060a4317014e5966075eba11289
Chris Vanderloo <chris.vanderloo@yahoo.com>
2019-04-16 14:32:06 -0400

    Added Schedule rendering separation

--- views/Schedule.js
+++ views/Schedule.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Text, View, SafeAreaView, ActivityIndicator, ScrollView } from 'react-native';
+import { Text, View, SafeAreaView, ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native';
 import { LargeNavBar, UpNext, RoundedCard } from '../components';
 import { EventRegister } from 'react-native-event-listeners';
 import { Col, Row, Grid } from "react-native-easy-grid";
+import { FN } from "../styles";
+import moment from "moment";
 import globals from "../globals.js";
+
+var hoursArr = ['8 AM','9 AM','10 AM','11 AM','12 PM'];
+for (var i=1; i<10; i++) hoursArr.push(i + ' PM');
+const weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday'];
 
 export default class ScheduleScreen extends React.Component {
   constructor(props) {
@@ -16,10 +22,16 @@ export default class ScheduleScreen extends React.Component {
 
   componentWillMount() {
     if (globals.SCHEDULE.loaded) 
-      this.setState({ loading: false, schedule: globals.SCHEDULE });
+      this.setState({ 
+        loading: false, 
+        schedule: globals.SCHEDULE
+      });
     else {
       EventRegister.addEventListener('load_schedule', (data) => {
-        this.setState({ loading: false, schedule: data });
+        this.setState({ 
+          loading: false, 
+          schedule: data 
+        });
       });
     }
   }
@@ -32,11 +44,24 @@ export default class ScheduleScreen extends React.Component {
     );
   }
 
+  generateDayCol(dayCl, i) {
+      var classes = [];
+      for (var j in hoursArr) {
+        classes.push(
+          <Row size={2} key={j} style={{
+            borderTopColor: '#CCCCCC',
+            borderTopWidth: 1
+          }}><Text></Text></Row>
+        );
+      }
+      return classes;
+  }
+
   render() {
-    var hours = ['8am','9am','10am','11am','12pm'];
-    for (var i=1; i<10; i++) hours.push(i + 'pm');
-    var weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday'];
     const clinfo = this.state.schedule.clinfo;
+    const table = weekdays.map((weekday, i) => this.generateDayCol(clinfo[i]));
+    const WeekdaySchedule = (props) => table[props.i];
+    // globals.get_class_info({ CRN:73948 }).then(res=>console.log(res));
     return (
     	<React.Fragment>
         <LargeNavBar title={'Schedule'} gearHidden={true}/>
@@ -44,39 +69,39 @@ export default class ScheduleScreen extends React.Component {
 	      {!this.state.loading &&
           <SafeAreaView> 
             <Grid>
-              <Row style={{ backgroundColor: '#F0F8FF', height: 300 }}>
+              <Row style={{ height: 350 }}>
                 <Col style={{ width: 60 }}>
-                  <Row size={1}></Row>
-                  {hours.map((obj, i) => 
+                  <Row size={1.5}></Row>
+                  {hoursArr.map((obj, i) => 
                     <Row size={2} key={i} style={{ 
                         flexDirection: 'column', 
                         alignItems: 'flex-end',
                         color: '#333333',
-                        paddingRight: 5
+                        paddingRight: 5,
+                        color: '#7C7C7C'
                     }}>
-                      <Text>{obj}</Text>
+                      <Text style={{fontSize:FN(14)}}>{obj}</Text>
                     </Row>
                   )}
-                  <Row size={1}></Row>
+                  <Row size={0.5}></Row>
                 </Col>
                 <Col>
                   <ScrollView horizontal={true}>
                     <Grid style={{ 
                       width: 700, 
-                      height: 400, 
-                      backgroundColor: '#EFEFEF' 
+                      height: 350
                     }}>
                       {weekdays.map((obj, i) => 
                         <Col key={i}>
-                          <Row size={1}>
-                            <Text>{obj}</Text>
+                          <Row style={{
+                            flexDirection: 'column', 
+                            alignItems: 'center'
+                          }} size={2}>
+                            <Text style={{
+                              color:'#7C7C7C'
+                            }}>{obj}</Text>
                           </Row>
-                          {hours.map((obj,i) => 
-                            <Row size={2} key={i} style={{
-                              borderTopColor: '#BBBBBB',
-                              borderTopWidth: 1
-                            }}></Row>
-                          )}
+                          <WeekdaySchedule i={i} />
                         </Col>
                       )}
                     </Grid>
