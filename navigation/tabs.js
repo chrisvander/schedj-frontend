c9a1a3e5d6082a6ceb33421896153da2923860f1
Chris Vanderloo <chris.vanderloo@yahoo.com>
2019-04-29 00:53:04 -0400

    linted tabs.js

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,16 +1,16 @@
+/*  eslint global-require: "off"  */
 import React from 'react';
-import { Feed, Schedule, Profile } from "../views";
-import { Grades } from "../views/profile";
-import { Manage } from "../views/schedule";
-import { createBottomTabNavigator, createAppContainer, createStackNavigator } from 'react-navigation';
-import { StyleSheet, View, Image } from 'react-native';
-import { IconComponent } from '../components';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import { StyleSheet, Image } from 'react-native';
+import { Feed, Schedule, Profile } from '../views';
+import { Grades } from '../views/profile/index';
+import { Manage } from '../views/schedule/index';
 
 const s = StyleSheet.create({
   tabIcon: {
     width: 21,
-    height: 21
-  }
+    height: 21,
+  },
 });
 
 const navOptions = {
@@ -19,16 +19,16 @@ const navOptions = {
       backgroundColor: '#FFF',
       elevation: 0,
       shadowOpacity: 0.3,
-      borderBottomColor:'transparent',
-      borderBottomWidth: 0
+      borderBottomColor: 'transparent',
+      borderBottomWidth: 0,
     },
     headerTintColor: '#2699FB',
     headerTitleStyle: {
       fontWeight: 'bold',
       fontFamily: 'Helvetica Neue',
-      fontSize: 14
-    }
-  }
+      fontSize: 14,
+    },
+  },
 };
 
 const ProfileStack = createStackNavigator({
@@ -36,8 +36,8 @@ const ProfileStack = createStackNavigator({
     screen: Profile,
     navigationOptions: {
       header: null,
-      gesturesEnabled: false
-    }
+      gesturesEnabled: false,
+    },
   },
   Grades: {
     screen: Grades,
@@ -49,53 +49,52 @@ const FeedStack = createStackNavigator({
     screen: Feed,
     navigationOptions: {
       header: null,
-      gesturesEnabled: false
-    }
+      gesturesEnabled: false,
+    },
   },
   Manage: {
     screen: Manage,
-  }
+  },
 }, navOptions);
 
 const TabNavigator = createBottomTabNavigator(
   {
     Feed: FeedStack,
-    Schedule: Schedule,
-    Profile: ProfileStack
+    Schedule,
+    Profile: ProfileStack,
   },
   {
-  	defaultNavigationOptions: ({ navigation }) => ({
-  		tabBarIcon: ({ focused, horizontal, tintColor }) => {
-  			const { routeName } = navigation.state;
-        if (routeName === "Feed") 
-          return focused ? 
-            <Image style={s.tabIcon} source={require('../assets/tabbar/feed_on-3x.png')} /> :
-            <Image style={s.tabIcon} source={require('../assets/tabbar/feed-3x.png')} />;
-        else if (routeName === "Schedule") 
-          return focused ?
-            <Image style={s.tabIcon} source={require('../assets/tabbar/calendar_on-3x.png')} /> :
-            <Image style={s.tabIcon} source={require('../assets/tabbar/calendar-3x.png')} />;
-        else 
-          return focused ?
-            <Image style={s.tabIcon} source={require('../assets/tabbar/profile_on-3x.png')} /> :
-            <Image style={s.tabIcon} source={require('../assets/tabbar/profile-3x.png')} />;
-        
-  		},
-  	}),
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused }) => {
+        const { routeName } = navigation.state;
+        if (routeName === 'Feed') {
+          return focused
+            ? <Image style={s.tabIcon} source={require('../assets/tabbar/feed_on-3x.png')} />
+            : <Image style={s.tabIcon} source={require('../assets/tabbar/feed-3x.png')} />;
+        }
+        if (routeName === 'Schedule') {
+          return focused
+            ? <Image style={s.tabIcon} source={require('../assets/tabbar/calendar_on-3x.png')} />
+            : <Image style={s.tabIcon} source={require('../assets/tabbar/calendar-3x.png')} />;
+        }
+        return focused
+          ? <Image style={s.tabIcon} source={require('../assets/tabbar/profile_on-3x.png')} />
+          : <Image style={s.tabIcon} source={require('../assets/tabbar/profile-3x.png')} />;
+      },
+    }),
     tabBarOptions: {
-    	tabBarPosition: 'bottom',
+      tabBarPosition: 'bottom',
       activeTintColor: '#FFFFFF',
       inactiveTintColor: '#68B8FC',
       showLabel: false,
       style: {
-      	backgroundColor: '#2699FB'
-      }
+        backgroundColor: '#2699FB',
+      },
     },
     animationEnabled: true,
     swipeEnabled: true,
-    initialRouteName: 'Feed'
-  }
+    initialRouteName: 'Feed',
+  },
 );
 
 export default TabNavigator;
-
